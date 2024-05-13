42cf1c9fb09eee233e8976131010055efb0a4684
Angel Rodriguez <venegasrv@outlook.com>
2024-05-13 09:23:20 -0600

    Admin sidervar white

--- resources/views/admin/perfil.blade.php
+++ resources/views/admin/perfil.blade.php
@@ -20,10 +20,6 @@
 
         @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
         <x-section-border />
-
-        <div class="mt-10 sm:mt-0">
-            @livewire('profile.delete-user-form')
-        </div>
     @endif
     </div>
 </x-admin-layout>
