20902f4f36cd7d1b299a6d99f23121823fd33dd2
raziel-toscano11 <razi.tuf11@gmail.com>
2024-06-19 17:24:07 -0600

    cambio del titulo

--- resources/views/livewire/navbar-proveedor.blade.php
+++ resources/views/livewire/navbar-proveedor.blade.php
@@ -18,7 +18,7 @@
                     </svg>
                 </button>
                 <a href="{{ route('proveedor.dashboard') }}" class="flex ms-2 md:me-24">
-                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">ServiMex</span>
+                    <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap">HandyMex</span>
                 </a>
             </div>
             <div class="flex items-center">
